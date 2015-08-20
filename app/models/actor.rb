cf3d734e6a4362651fae39c7899a09124ed9d4d7
George Toothman <george.toothman@flatironschool.com>
2015-08-20 16:47:48 -0400

    add model associations and migrations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    # we have two columns, first_name and last_name
+    # how to inject data into a string - interpolate
+    "#{self.first_name} #{self.last_name}"
+  end
   
+  def list_roles
+    # "Khaleesi - Game of Thrones"
+    characters.map do |character|
+      "#{character.name} - #{character.show.name}"
+    end.join(" ")
+  end
 end
